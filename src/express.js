391f8b89835991ded443af788bc6b06f5b0bc442
mehedi-hasan <mdmehedihasan2810@gmail.com>
2024-01-09 23:09:08 +0600

    feat: add layer and express api

--- src/express.js
+++ src/express.js
@@ -1,9 +1,17 @@
+var mixin = require('merge-descriptors');
+var proto = require("./app")
+
 exports = module.exports = createApplication;
 
-function createApplication(){
-    var app = function(req, res, next){
+function createApplication() {
+    let app = function(req,res,next) {
+        app.handle(req,res,next)
+    };
 
-    }
+    mixin(app,proto,false);
 
+    app.init();
     return app;
 }
+
+exports.application = proto;
