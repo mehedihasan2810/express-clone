a5ef4361dc0c9ac9529151236e323763e1f21050
mehedi-hasan <mdmehedihasan2810@gmail.com>
2024-01-09 23:08:07 +0600

    feat: add application api

--- src/app.js
+++ src/app.js
@@ -1,30 +1,66 @@
-var app = (exports = module.exports = {});
-var methods = require("methods");
-
+var setPrototypeOf = require('setprototypeof')
+var methods = require('methods');
+var Router = require('./router');
+var Layer = require('./Layer')
 var slice = Array.prototype.slice;
+var http = require('http');
 
-app.init = function () {
-  this.cache = {};
-  this.engines = {};
-  this.settings = {};
+var app = exports = module.exports = {};
 
-  //for holding the application router
-  this._router = undefined;
+app.init = function() {
+    this.cache = {};
+    this.engines = {};
+    this.settings = {}
+
+    this._router = undefined;
+};
+
+app.set = function set(setting,val) {
+    this.settings[setting] = val;
+
+    switch (setting) {
+        case 'etag':
+            this.set('etag fn',"")
+            break;
+        case 'query parser':
+            this.set('query parser fn',"")
+            break
+        case 'trust proxy':
+            this.set('trust proxy fn',"");
+            break;
+    }
+
+    return this;
+};
+
+app.enabled = function enabled(setting) {
+    return Boolean(this.set(setting));
 };
 
 app.lazyrouter = function lazyrouter() {
-  if (!this._router) {
-    this._router = new Router({});
-  }
+    if(!this._router) {
+        this._router = new Router({})
+    }
 };
 
-methods.forEach(function (method) {
-  app[method] = function (path) {
-    this.lazyrouter();
+app.listen = function listen() {
+    var server = http.createServer(this);
+    return server.listen.apply(server, arguments);
+};
 
-    var route = this._router.route(path);
+app.handle = function handle(req, res, callback) {
+    var router = this._router;
 
-    route[method].apply(route, slice.call(arguments, 1));
-    return this;
-  };
+    router.handle(req, res);
+};
+
+methods.forEach(function (method){
+    app[method] = function(path) {
+        this.lazyrouter()
+
+        var route = this._router.route(path);
+
+        route[method].apply(route, slice.call(arguments, 1));
+        return this;
+    }
 });
